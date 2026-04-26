3502c1866d1501671911af47911bf4d6a784dc61
jitin14ry <jitin14ry@gmail.com>
2026-04-26 20:52:02 +0530

    api error , api response , async handler and notes created

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cros";
+import cookieParser from "cookie-parser";
+
+// app.use tab use krnge jab koi middleware ya configurations setting krni h
+
+const app = express();
+
+// defining cors
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credential: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+export { app };
---
+++ utils/asyncHandler.js
@@ -0,0 +1,30 @@
+// promises func
+
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+// OR
+
+const asyncHandler = (requestHandler) => (req, res, next) => {
+  Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+};
+
+export { asyncHandler };
+
+// try catch func
+
+/*
+const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    res.status(error.code || 500).json({
+      sucess: false,
+      message: error.message,
+    });
+  }
+};
+*/
